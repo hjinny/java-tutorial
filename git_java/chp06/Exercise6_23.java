facba5cc6857ce1a27c868d89caec36ae971a60d
jinny7595 <jinny7595@gmail.com>
2015-06-23 17:51:59 +0900

    NullPointException 원인 파악

--- git_java/chp06/Exercise6_23.java
+++ git_java/chp06/Exercise6_23.java
@@ -1,12 +1,17 @@
 package chp06;
 
-class Exercise6_23
-{
+class Exercise6_23{
 	public static int max(int[] num){
 		int max = 0;
 		
 		if(num==null || num.length==0){
-			max = -999999;
+			
+			max = -99999;
+			
+			/* null값인데 return 하지 않으면 NullPointException 오류가 뜸
+			 * 1)return max;
+			 * 2)return -999999;
+			 */
 		}
 		for(int i=0; i<num.length; i++){
 			if(num[i]> max){
@@ -17,12 +22,13 @@ class Exercise6_23
 	}
 	
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args){
 		int[] data = {3,2,9,4,7};
+	
 		System.out.println(java.util.Arrays.toString(data));
 		System.out.println("최대값:"+max(data));
 		System.out.println("최대값:"+max(new int[]{})); // 크기가 0인 배열
+		
 		System.out.println("최대값:"+max(null));
 	}
 }
